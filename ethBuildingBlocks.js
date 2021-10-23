e8e9f1d9e2a338eaf95a234d72cbea01bc09b0bf
Javier Barrios <73854785+MuayThaiLegz@users.noreply.github.com>
2021-10-22 17:30:42 -0700

    more content
    updated content for individual compartments

--- ethBuildingBlocks.js
+++ ethBuildingBlocks.js
@@ -32,3 +32,71 @@ withdraw_amount = web.toWei(0.01, 'ether');
 withdraw_amount_hex = web3.toHex(withdraw_amount);
 
 
+// Transactioon context
+// The tx object provides a means of accessing transsaction-realted information
+
+tx.gasPrice
+    // The gas price in the calling transaction.
+
+tx.origin
+    // The address of the originating EOA for this transaction. WARNING; unsafe!
+
+// Block context 
+
+    // The block object contains information about the current block
+
+block.blockhash(blockNumber)
+    // The block hash of the specified block number, up to 256 blocks in the past.
+    // Doprecates and repalced with the blockhash function
+
+block.coinbase
+    // The address of the recipient of the current block's fees and block reward.
+
+block.difficulty
+    // The difficulty (proof of work) of the current block
+
+block.gaslimit
+    // The max amount of gas that can be spent across all transacntions included in the cuurent block.
+
+block.blockNumber
+    // The current block number (blockchain height)
+
+block.timestamp
+    // The timestamp placed in the current blocl by the miner ( number of seconds since the Unix epoch)
+
+// Address Object 
+// Any address, either passed as an inout or cast from a contract pbject, has a number of sttribuytes and methods
+
+address.balance 
+    // The balance of the address, in wei, EX current contract balance is address(this).balance
+
+address.tranfer(amount) 
+    // Tranfoers the amount (in wei) to this address, throwing an exception on any error. We used this fucntion in our Faucet EX
+
+address.send(amount)
+    // Similiar to tranfer, only instead of trhowing an eception, it returns false on error.
+        // WARNING always check the return value of send.
+
+address.call(payload)
+    // Low-level call futnion-0 can construct an arbitrary message call with a data payload. Retuns false on error. Unsafe.
+
+address.delagatecall()
+    // Low-level Delegatecall fucntion, like callcode(...) but with the full msg context senn by the curr contract.
+        // Returns false on erroe WARNING: advanced use only
+
+// Built-in functions
+// Other fucntions worth noting are:
+
+addmod, mulmod
+ // # For modulo addition and multiplication. EX addmod(x,y,k) calculates (x + y) % k
+
+ ecrecoer
+    // Recovers the address used to sign a message from the sinature
+
+selfdestruct(recipient_address)
+    // Deletes the current contract, sending any remaingn  eth in the account to the recipients address 
+
+this
+    // The address of the currently executing contract account
+
+// 

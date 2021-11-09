fd54c3397b65a441a04fb37dd7e169cb0180947c
Javier Barrios <73854785+MuayThaiLegz@users.noreply.github.com>
2021-11-08 17:20:26 -0800

    adding come ERC contract proposals
    coded along with different erc toke standards

--- SmartContractSecurity.sol
+++ SmartContractSecurity.sol
@@ -124,5 +124,104 @@ contract  Fibonaccililb {
 
 // Lets now consider a contract that use this lib 
 
-//contract FibBal {
-  //  address public fi
+pragma solidity 0.7.0;
+/*
+contract insta {
+    
+    function isContract(address _addr) private returns (bool is_contract) { 
+        uint length;
+            assembly {
+            // retries the size of the code on target address; this needs assembly
+                length := extcodesize(_addr)
+        }
+        return (length>0)
+    }
+}
+*/
+contract interface_ERC223Token {
+        uint public totalSupply;
+        function balanceOf(address who) public view returns (uint);
+
+        function name() public view returns (string _name);
+        function symbol() public view returns (string _symbol);
+        function decimals() public view returns (uint8 _decimals);
+        function tottalSupply() public view returns (uint256 _supply);
+
+        function transfer(address to, uint value) public returns (bool ok);
+        function trasnfer(address to, uint value, bytes data) public returns (bool ok);
+        function trasnfer(address to, uint value, bytes data, string custom_fallback)
+            public returns (bool ok);
+
+        event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+}
+    
+
+// ERC77: A Proposed Token Contract Interface Standard: The ERC777 contract interaface specifiacation:
+
+contract interface_ERC777Token{
+    function name() public constant returns (string);
+    function symbol() public constant returns (string);
+    function totalSupply() public constant returns (uint256);
+    function granlarity() public contract returns (uint256);
+    function balanceOf(address owner) public constant returns (uint256);
+
+    function send(address to, uint256 amount, bytes userData) public;
+
+    function authorizeOperator(address operator) public;
+    function revokeOperator(address operator) public;
+    function inOperatorFor(address operator, address tokenHolder)
+        public constant returns (bool);
+    function operatorSend(address from, address to, uint256 amount, bytes underData, bytes operatorData) public;
+
+    event Sent(address indexed operator, address indexed from, address indexed to, uint256 amount,\
+    bytes userData, bytes userData, bytes operatorData);
+
+    event Burned(address indexed operator, address indexed from, uint256 amount, bytes userData, bytes operatorData);
+
+    event AuthorizedOperatior(address indexed operator, address indexed tokenHolder);
+
+    event RevokedOperator(address indexed operator,  address indexed tokenHolder);
+
+
+// ERC777 hooks: The ERC777 tokens sender hook specifition is 
+
+interface ERC777TokenSender {
+    function tokensToSend(address operator, address from, address to , uint value,/
+     bytes userData, bytes operatorData) public;
+    }
+}
+
+
+// The ERC721 contract interface specification is:
+
+contract interface_ERC721{ 
+    event Tranfer(address indexed _from, address indexed _to, uint256 _deedId);
+    event Approval(address indexed _owner, address indexed _approved, bool _approved);
+
+    function balanceOf(address _owner) external view returns (uint256 _balance);
+    function ownerOf(uint256 _deedId) external view returns (address _owner);
+    function transfer(address _to, uint256 _deedId) external payable;
+    function transferFrom(address _from, address _to, uint256 _deedId) external payable;
+    function approve(address _approved, uint256 _deedId) external payable;
+    function setApprovalForAll(address _operator, boolean _approved) payable;
+    function supportInterface(bytes4 interfaceID) external view returns (bool);
+
+    // ERC721 also supports two optinal interfaces, one for metadata and one for enumeration of deeds and owners
+}
+
+contract interface_ERC721MetaData { // is erc721
+    function name() external pure returns (string _name);
+    function symbol() external pure returns (string _symbols);
+    function deedUrl(uint256 _deedId) external view returns (string _deedUrl);  
+}
+// ERC721 optinal interface for enumerations is 
+
+contract interface_ERC721Enumerable{ // is rec721
+    function totalSunply() external view returns (uint256 _count);
+    function deedByIndex(uint256 _index) external view returns (uint265 _deedId);
+    function countOfOwners() external view returns (uint256 _count);
+    function ownerByIndex(uint256 _index) external view returns (address _owner);
+    function deedOfOwnerByIndex(address _owner, uint265 _index) external view returns (uint256 _deedId);
+}
+
+

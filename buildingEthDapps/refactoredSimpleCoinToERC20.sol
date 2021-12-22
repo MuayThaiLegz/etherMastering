8a1bb6eb7e0f22d569377e315d060210a16ae476
Javier Barrios <73854785+MuayThaiLegz@users.noreply.github.com>
2021-12-22 11:35:30 -0800

    completed contract
    had to improvise but excited to move on to working with web3.js

--- buildingEthDapps/refactoredSimpleCoinToERC20.sol
+++ buildingEthDapps/refactoredSimpleCoinToERC20.sol
@@ -47,5 +47,67 @@ contract SimpleCoin is Ownable, ERC20 {
         mint(owner, _initialSupply);
     }
 
+    function balanceOf(address _account)
+        public view returns (uint256 balance) {
+        return coinBalance[_account];
+    }
+
+    function transfer(address, _to, uint256 _amount) public returns (bool) {
+        require(_to != 0x0);
+        require(CoinBalance[msg.sender] > _amount);
+        require(CoinBalance[_to] + _amount >= coinBalance[_to]);
+        CoinBalance[msg.sender] -= _amount;
+        CoinBalance[_to] += _amount;
+        emit Transfer(msg.sender, _to, _amount);
+        return true;
+    }
+
+    function approve(address _authorizedAccount, uint256 _allowance)
+        public returns (bool success) {
+            allowances[msg.sender][_authorizedAccount] = _allowance;
+            emit Approval(msg.sender, _authorizedAccount, _allowance);
+            return true;
+    }
+
+    function transferFrom(address, _from,address, _to, uint256 _amount) public returns (bool success) {
+        require(_to != 0x0);
+        require(CoinBalance[_from] > _amount);
+        require(CoinBalance[_to] + _amount >= coinBalance[_to]);
+        require(_amount <= allowances[_from][msg.sender]);
+
+        CoinBalance[msg.sender] -= _amount;
+        CoinBalance[_to] += _amount;
+        emit Transfer(_from, _to, _amount);
+        return true;
+    }
+
+    function allowance(address _authorizer, address _authorizedAccount) public view returns (uint256){
+
+        return allowances[_authorizer][_authorizedAccount];
+    }
+
+    function mint(address _recipient, uint256 _mintedAmount) onlyOwner public {
+        coinBalance[_recipient] += _mintedAmount;
+        emit Transfer(owner, _recipient, _mintedAmount);
+    }
+
+    function freezeAccount(address target, bool freeze)
+        onlyOwner public {
+
+        frozenAccount[target] = freeze;
+        emit FrozenAccount(target, freeze);
+    }
+/* 
+    Althought the token constructor isn't part of the interface and consequently itn't 
+    included in the standard, ERC20 recommends initializing a token with the following info.
+
+    string public constant name = "Token Name";
+
+    string public constant symbol = "MAN";
+
+    uint8 public constant decimals = 18;
     
+
+
+
 }
